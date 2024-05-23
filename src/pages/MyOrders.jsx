e10d0e8629fbdf4ed8cb425d156e085d8fc74fef
hatim85 <142659084+hatim85@users.noreply.github.com>
2024-05-23 11:16:28 +0530

    Update MyOrders.jsx

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -57,36 +57,57 @@ function MyOrders() {
         {error && <p className="text-red-600">Error: {error}</p>}
         {orders && Object.keys(orders).length > 0 ? (
           <div>
-            {Object.values(orders).map((order) => (
-              <div key={order._id} className="my-6 border relative border-gray-300 rounded-lg p-4">
-                <div className="flex justify-between items-center mb-4">
-                  <div className="bg-gray-200 rounded-full px-4 py-2 text-md font-semibold">
-                    Order ID: {order._id}
+            {Object.values(orders)
+              .sort((a, b) => new Date(b.orderDate) - new Date(a.orderDate))
+              .map((order) => (
+                <div key={order._id} className="my-6 border relative border-gray-300 rounded-lg p-4">
+                  <div className="flex justify-between items-center mb-4">
+                    <div className="bg-gray-200 rounded-full px-4 py-2 text-md font-semibold">
+                      Order ID: {order._id}
+                    </div>
+                    <p className="text-sm text-gray-500">
+                      Order Placed on:{' '}
+                      {new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: '2-digit' }).format(
+                        new Date(order.orderDate)
+                      )}
+                    </p>
                   </div>
-                  <p className="text-sm text-gray-500">Order Placed on: {new Intl.DateTimeFormat('en-GB', { day: '2-digit', month: '2-digit', year: '2-digit' }).format(new Date(order.orderDate))}</p>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                  {order.products && order.products.length > 0 && order.products.map((product) => {
-                    const { productId } = product;
-                    return (
-                      <div key={product._id} className="border border-gray-300 rounded-lg p-4 flex md:gap-10 md:flex-row flex-col md:items-start items-center justify-around">
-                        <Link to={`/products/${productId._id}`} className='md:flex'>
-                          <img src={productId.image[0] || "../../ErrorImage.png"} alt="product" className="w-24 h-24 object-cover mb-2 md:mr-20" />
-                          <div className="text-center">
-                            <p className="font-semibold">{productId.name}</p>
-                            <p>Quantity: {product.quantity}</p>
-                            <p>Price: ₹{productId.price}</p>
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    {order.products &&
+                      order.products.length > 0 &&
+                      order.products.map((product) => {
+                        const { productId } = product;
+                        return (
+                          <div
+                            key={product._id}
+                            className="border border-gray-300 rounded-lg p-4 flex md:gap-10 md:flex-row flex-col md:items-start items-center justify-around"
+                          >
+                            <Link to={`/products/${productId._id}`} className="md:flex">
+                              <img
+                                src={productId.image[0] || '../../ErrorImage.png'}
+                                alt="product"
+                                className="w-24 h-24 object-cover mb-2 md:mr-20"
+                              />
+                              <div className="text-center">
+                                <p className="font-semibold">{productId.name}</p>
+                                <p>Quantity: {product.quantity}</p>
+                                <p>Price: ₹{productId.price}</p>
+                              </div>
+                            </Link>
                           </div>
-                        </Link>
-                      </div>
-                    );
-                  })}
-                  <button onClick={() => handleCancelOrder(order._id)} className='text-white px-4 py-2 bg-red-500 absolute bottom-5 right-5 rounded-xl'>Cancel</button>
+                        );
+                      })}
+                    <button
+                      onClick={() => handleCancelOrder(order._id)}
+                      className="text-white px-4 py-2 bg-red-500 absolute bottom-5 right-5 rounded-xl"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                  <p className="mt-4">Order Status: {order.status}</p>
+                  <p className="font-bold mt-2">Total Amount: ₹{order.totalAmount}</p>
                 </div>
-                <p className="mt-4">Order Status: {order.status}</p>
-                <p className="font-bold mt-2">Total Amount: ₹{order.totalAmount}</p>
-              </div>
-            ))}
+              ))}
           </div>
         ) : (
           <p className="text-gray-600">No orders found.</p>
