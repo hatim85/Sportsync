420875258f751039e1773430ca2ca0877fdf71d8
hatim85 <142659084+hatim85@users.noreply.github.com>
2024-07-18 00:21:52 +0530

    Corrected price

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -50,7 +50,7 @@ function ProductCard() {
               <div className="p-4">
                 <h2 className={` font-bold ${product.name.length > 16 ? 'sm:text-sm md:text-xl' : 'md:text-xl'}`}>{product.name}</h2>
                 <p className={`text-gray-700 ${product.description.length > 20 ? 'text-sm' : 'mt-2 md:text-xl sm:text-sm'}`}>{product.description.length > 20 ? product.description.substring(0, 10) + '...' : product.description}</p>
-                <h2 className="md:text-lg text-sm font-semibold">₹{product.price} <del className='text-gray-500 text-sm'>${product.price + 200}</del></h2>
+                <h2 className="md:text-lg text-sm font-semibold">₹{product.price} <del className='text-gray-500 text-sm'>₹{product.price + 200}</del></h2>
               </div>
             </Link>
           </div>
